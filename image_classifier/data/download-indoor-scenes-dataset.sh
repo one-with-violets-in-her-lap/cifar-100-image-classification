e976a9cabbb94d0c13a1776818ef2e258476e1b9
crucials <ensueness@gmail.com>
2025-02-18 16:49:39 +0500

    implement train/test split lists loading

---
+++ image_classifier/data/download-indoor-scenes-dataset.sh
@@ -0,0 +1,4 @@
+mkdir ./datasets
+curl -L -o ./datasets/indoor-scenes-cvpr-2019.zip \
+  https://www.kaggle.com/api/v1/datasets/download/itsahmad/indoor-scenes-cvpr-2019
+unzip ./datasets/indoor-scenes-cvpr-2019.zip -d ./datasets/indoor-scenes-cvpr-2019
